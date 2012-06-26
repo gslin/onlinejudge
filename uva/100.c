8208a7a91e1e120da2cd07f1533c9d2ebd2cba0f
Gea-Suan Lin <gslin@gslin.org>
2012-06-27 06:32:09 +0800

    uva/100.c first version

---
+++ uva/100.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+unsigned int count_length(unsigned int x)
+{
+    unsigned int count;
+
+    for (count = 1; 1 != x; count++) {
+        if (0 == x % 2) {
+            x /= 2;
+        } else {
+            x = 3 * x + 1;
+        }
+    }
+
+    return count;
+}
+
+int main(int argc, const char *argv[])
+{
+    unsigned int x, y;
+    while (0 < scanf("%d %d", &x, &y)) {
+        unsigned int end, start;
+        if (x < y) {
+            start = x;
+            end = y;
+        } else {
+            start = y;
+            end = x;
+        }
+
+        unsigned int i, max;
+        for (i = start; i <= end; i++) {
+
+            unsigned int n = count_length(i);
+            if (n > max) {
+                max = n;
+            }
+        }
+
+        printf("%d %d %d\n", start, end, max);
+    }
+
+    return 0;
+}
