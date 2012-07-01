10af470eac4f1e2e3913391d031c4fa65ebd0f4d
Gea-Suan Lin <gslin@gslin.org>
2012-07-02 02:21:50 +0800

    Implement string reverse

--- uva/113.c
+++ uva/113.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+void reverse(char *str)
+{
+    char *start = str, *end = str + strlen(str) - 1;
+
+    while (start < end) {
+        *start ^= *end;
+        *end ^= *start;
+        *start ^= *end;
+
+        start++;
+        end++;
+    }
+}
 
 int main(int argc, const char *argv[])
 {
