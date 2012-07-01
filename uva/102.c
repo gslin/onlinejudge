9152488391c70e40a5963a2cfdbf98bdef7c18da
Gea-Suan Lin <gslin@gslin.org>
2012-07-02 01:55:43 +0800

    Add 6 pre-defined output strings

--- uva/102.c
+++ uva/102.c
@@ -7,6 +7,7 @@
 int main(int argc, const char *argv[])
 {
     unsigned int num[9];
+    const char *result[] = {"BCG", "BGC", "CBG", "CGB", "GBC", "GCB"};
 
     while (0 < scanf("%u %u %u %u %u %u %u %u %u", &num[0], &num[2], &num[1], &num[3], &num[5], &num[4], &num[6], &num[8], &num[7])) {
         int i;
