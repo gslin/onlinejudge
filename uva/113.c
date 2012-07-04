24cb2c6a9e5f885ef4b4fdf62bf49ddd9634c8b2
Gea-Suan Lin <gslin@gslin.org>
2012-07-05 01:06:51 +0800

    Use log10() to calculate

--- uva/113.c
+++ uva/113.c
@@ -1,4 +1,6 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, const char *argv[])
@@ -11,6 +13,21 @@ int main(int argc, const char *argv[])
             break;
         }
         scanf("%s", p);
+
+        /* XXX We might need to filter p */
+
+        double log10_p;
+        if (strlen(p) > 9) {
+            char t[10];
+            strncpy(t, p, sizeof(t) - 1);
+            log10_p = log10(atoi(t)) + strlen(p) - 9;
+        } else {
+            log10_p = log10(atoi(p));
+        }
+
+        double t = log10_p / n;
+
+        printf("%u\n", (unsigned int) (pow(10, t) + 0.5));
     }
 
     return 0;
