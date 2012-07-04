1c01538c0436ae5b8c45085077a465e5c3a3000e
Gea-Suan Lin <gslin@gslin.org>
2012-07-05 01:27:42 +0800

    "double" range is enough to solve this issue, we dont need to implement bignum operation

--- uva/113.c
+++ uva/113.c
@@ -1,33 +1,18 @@
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main(int argc, const char *argv[])
 {
-    unsigned int n;
-    char p[128];
-
     for (;;) {
+        unsigned int n;
+        double p;
+
         if (0 >= scanf("%u", &n)) {
             break;
         }
-        scanf("%s", p);
+        scanf("%lf", &p);
 
-        /* XXX We might need to filter p */
-
-        double log10_p;
-        if (strlen(p) > 9) {
-            char t[10];
-            strncpy(t, p, sizeof(t) - 1);
-            log10_p = log10(atoi(t)) + strlen(p) - 9;
-        } else {
-            log10_p = log10(atoi(p));
-        }
-
-        double t = log10_p / n;
-
-        printf("%u\n", (unsigned int) (pow(10, t) + 0.5));
+        printf("%u\n", (unsigned int) (pow(p, 1.0 / n) + 0.5));
     }
 
     return 0;
