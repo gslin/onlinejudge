01c8fa318ce8334ce953b5d3b1041e30fa704b4f
Gea-Suan Lin <gslin@gslin.org>
2012-07-05 01:06:28 +0800

    Remove reverse()

--- uva/113.c
+++ uva/113.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverse(char *str)
-{
-    char *start = str, *end = str + strlen(str) - 1;
-
-    while (start < end) {
-        /* This is actually swap hack */
-        *start ^= *end;
-        *end ^= *start;
-        *start ^= *end;
-
-        start++;
-        end++;
-    }
-}
-
 int main(int argc, const char *argv[])
 {
     unsigned int n;
@@ -26,7 +11,6 @@ int main(int argc, const char *argv[])
             break;
         }
         scanf("%s", p);
-        reverse(p);
     }
 
     return 0;
